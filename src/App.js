6eac1a903138da92809e10f2664a0c4b0bcc7d46
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-26 17:35:03 +0530

    change dynamic style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserForm from './components/users/UserForm';
+import UserList from './components/users/UserList';
+
 
 function App() {
+  const [usersList ,setUsersList] = useState([]);
+
+  const userDataHandler = (data) => {
+    setUsersList((previousStateData) => {
+      return [data, ...previousStateData]
+    })
+  }
   return (
-    <div>
-      <UserForm/>
-    </div>
-  );
+    <>
+      <UserForm onSaveData={userDataHandler}/>
+      <UserList users={usersList}/>
+    </>
+  )
 }
 
 export default App;
--- src/components/users/UserForm.js
+++ src/components/users/UserForm.js
@@ -1,27 +1,74 @@
-// import './UserForm.css'
+import './UserForm.css'
+import Card from "../UI/Card"
+import Button from '../UI/Button'
+import { useState } from 'react';
+import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 
-const UserForm = () => {
+const UserForm = (props) => {
 
-    const addUserHandler = (event) => {
-        event.preventDefault();
-    }
+	const [username, setUsername] = useState('');
+	const [age, setAge] = useState('');
+	const [error, setError] = useState();
 
-    return(
-            <form onSubmit={addUserHandler}>
-                
-                  
-                        <label htmlFor='username'>Username:</label>
-                        <input id='username' type='text'/>
-                
-                   
-                        <label htmlFor='age'>Age(Years)</label>
-                        <input id='age' type='number'/>
-                 
-                    <button type="submit">Add User</button>
-              
-            </form>
-    )
+	const onChangeNameHandler = (event) => {
+		setUsername(event.target.value)
+	}
 
-}
+	const onChangeAgeHandler = (event) => {
+		setAge(event.target.value)
+	}
 
-export default UserForm
+	const addUserHandler = (event) => {
+		event.preventDefault();
+
+		if(username.trim().length === 0 && age.trim().length === 0) {
+			setError({
+				title: 'Invalid Input',
+				message: 'please enter a valid name and age'
+			})
+			return
+		} 
+		if(+age < 1) {
+			setError({
+				title: 'invalid age',
+				message: 'please enter valid age'
+			})
+			return 
+		}
+		setUsername('')
+		setAge('')
+		const user = {
+			name: username,
+			age,
+			id: Math.random().toString()
+		}
+		props.onSaveData(user)
+
+	};
+
+	const errorHandler = () => {
+		setError(null);
+	}
+
+
+
+
+	return (
+		<Wrapper>
+		{error && <ErrorModal onConfirm={errorHandler} title={error.title} message={error.message}/>}
+		<Card className="input">
+			<form onSubmit={addUserHandler}>
+				<label htmlFor="username">Username:</label>
+				<input value={username} onChange={onChangeNameHandler} id="username" type="text" />
+
+				<label htmlFor="age">Age(Years)</label>
+				<input value={age} onChange={onChangeAgeHandler} id="age" type="number" />
+                <Button type="submit">Add User</Button>
+			</form>
+		</Card>
+		</Wrapper>
+	);
+};
+
+export default UserForm;
